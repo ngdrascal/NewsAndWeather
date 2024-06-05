0bd5c6944fc49947805466161ffa818a493cdafc
ngdrascal <rick.dodd@comcast.net>
2024-06-05 08:33:12 -0500

    incremental updates

--- WeatherDataClients/OpenWeather/OpenWeatherDto.cs
+++ WeatherDataClients/OpenWeather/OpenWeatherDto.cs
@@ -1,103 +1,126 @@
 ﻿/*
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-// ReSharper disable UnusedMember.Global
-// ReSharper disable InconsistentNaming
-
-using System.Text.Json.Serialization;
-
 namespace WeatherDataClients.OpenWeather;
 
-public class OpenWeather
+public class Rootobject
 {
-    public double Lat { get; set; }
-    public double Lon { get; set; }
+    public Class1[] Property1 { get; set; }
+}
+
+public class Class1
+{
+    public float Lat { get; set; }
+    public float Lon { get; set; }
     public string Timezone { get; set; }
-    [JsonPropertyName("timezone_offset")]
-    public int Timezone_Offset { get; set; }
-    public CurrentConditions Current { get; set; }
+    public int TimezoneOffset { get; set; }
+    public Current Current { get; set; }
+    public Minutely[] Minutely { get; set; }
     public Hourly[] Hourly { get; set; }
     public Daily[] Daily { get; set; }
     public Alert[] Alerts { get; set; }
 }
 
-public class CurrentConditions
+public class Current
 {
-    public long Dt { get; set; }
-    public long Sunrise { get; set; }
-    public long Sunset { get; set; }
-    public double Temp { get; set; }
-    [JsonPropertyName("Feels_Like")]
-    public FeelsLike Feels_Like { get; set; }
+    public int Dt { get; set; }
+    public int Sunrise { get; set; }
+    public int Sunset { get; set; }
+    public float Temp { get; set; }
+    public float FeelsLike { get; set; }
     public int Pressure { get; set; }
     public int Humidity { get; set; }
-    [JsonPropertyName("dew_point")]
-    public double Dew_Point { get; set; }
-    public double UltraVioletIndex { get; set; }
+    public float DewPoint { get; set; }
+    public float Uvi { get; set; }
     public int Clouds { get; set; }
     public int Visibility { get; set; }
-    [JsonPropertyName("wind_speed")]
-    public double Wind_Speed { get; set; }
-    [JsonPropertyName("wind_deg")]
-    public int Wind_Deg { get; set; }
-    [JsonPropertyName("wind_gust")]
-    public double Wind_Gust { get; set; }
+    public float WindSpeed { get; set; }
+    public int WindDeg { get; set; }
+    public float WindGust { get; set; }
     public Weather[] Weather { get; set; }
 }
 
-public class Hourly : CurrentConditions
+public class Weather
 {
-    public double Pop { get; set; }
+    public int Id { get; set; }
+    public string Main { get; set; }
+    public string Description { get; set; }
+    public string Icon { get; set; }
+}
+
+public class Minutely
+{
+    public int Dt { get; set; }
+    public int Precipitation { get; set; }
+}
+
+public class Hourly
+{
+    public int Dt { get; set; }
+    public float Temp { get; set; }
+    public float FeelsLike { get; set; }
+    public int Pressure { get; set; }
+    public int Humidity { get; set; }
+    public float DewPoint { get; set; }
+    public int Uvi { get; set; }
+    public int Clouds { get; set; }
+    public int Visibility { get; set; }
+    public float WindSpeed { get; set; }
+    public int WindDeg { get; set; }
+    public float WindGust { get; set; }
+    public Weather1[] Weather { get; set; }
+    public float Pop { get; set; }
+}
+
+public class Weather1
+{
+    public int Id { get; set; }
+    public string Main { get; set; }
+    public string Description { get; set; }
+    public string Icon { get; set; }
 }
 
 public class Daily
 {
-    public long Dt { get; set; }
-    public long Sunrise { get; set; }
-    public long Sunset { get; set; }
-    public long MoonRise { get; set; }
-    public long MoonSet { get; set; }
-    [JsonPropertyName("Moon_Phase")]
-    public double Moon_Phase { get; set; }
+    public int Dt { get; set; }
+    public int Sunrise { get; set; }
+    public int Sunset { get; set; }
+    public int Moonrise { get; set; }
+    public int Moonset { get; set; }
+    public float MoonPhase { get; set; }
     public string Summary { get; set; }
-    public Temperature Temp { get; set; }
-    [JsonPropertyName("Feels_Like")]
-    public FeelsLike Feels_Like { get; set; }
+    public Temp Temp { get; set; }
+    public FeelsLike FeelsLike { get; set; }
     public int Pressure { get; set; }
     public int Humidity { get; set; }
-    [JsonPropertyName("Dew_Point")]
-    public double Dew_Point { get; set; }
-    [JsonPropertyName("Wind_Speed")]
-    public double Wind_Speed { get; set; }
-    [JsonPropertyName("Wind_Deg")]
+    public float DewPoint { get; set; }
+    public float WindSpeed { get; set; }
     public int WindDeg { get; set; }
-    [JsonPropertyName("Wind_Gust")]
-    public double Wind_Gust { get; set; }
-    public Weather[] Weather { get; set; }
+    public float WindGust { get; set; }
+    public Weather2[] Weather { get; set; }
     public int Clouds { get; set; }
-    public double Pop { get; set; }
-    public double Rain { get; set; }
-    public double UltraVioletIndex { get; set; }
+    public float Pop { get; set; }
+    public float Rain { get; set; }
+    public float Uvi { get; set; }
 }
 
-public class Temperature
+public class Temp
 {
-    public double Day { get; set; }
-    public double Min { get; set; }
-    public double Max { get; set; }
-    public double Night { get; set; }
-    public double Eve { get; set; }
-    public double Morn { get; set; }
+    public float Day { get; set; }
+    public float Min { get; set; }
+    public float Max { get; set; }
+    public float Night { get; set; }
+    public float Eve { get; set; }
+    public float Morn { get; set; }
 }
 
 public class FeelsLike
 {
-    public double Day { get; set; }
-    public double Night { get; set; }
-    public double Eve { get; set; }
-    public double Morn { get; set; }
-}   
+    public float Day { get; set; }
+    public float Night { get; set; }
+    public float Eve { get; set; }
+    public float Morn { get; set; }
+}
 
-public class Weather
+public class Weather2
 {
     public int Id { get; set; }
     public string Main { get; set; }
@@ -109,8 +132,9 @@ public class Alert
 {
     public string SenderName { get; set; }
     public string Event { get; set; }
-    public long Start { get; set; }
-    public long End { get; set; }
+    public int Start { get; set; }
+    public int End { get; set; }
     public string Description { get; set; }
+    public object[] Tags { get; set; }
 }
 */
