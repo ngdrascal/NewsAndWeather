d295a3ebff2ed90ad4457961805f386b8a178fa4
ngdrascal <rick.dodd@comcast.net>
2024-11-17 12:16:22 -0600

    incremental updates

--- TimeAndTemperature/TimeAndTemperature/Program.cs
+++ TimeAndTemperature/TimeAndTemperature/Program.cs
@@ -1,5 +1,5 @@
-using TimeAndTemperature.Client.Pages;
 using TimeAndTemperature.Components;
+using WeatherDataClients;
 
 namespace TimeAndTemperature
 {
@@ -9,10 +9,32 @@ namespace TimeAndTemperature
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Configuration.AddEnvironmentVariables();
+
             // Add services to the container.
-            builder.Services.AddRazorComponents()
+            builder.Services
+                .AddRazorComponents()
                 .AddInteractiveServerComponents()
                 .AddInteractiveWebAssemblyComponents();
+
+            builder.Services.AddHttpClient("default");
+
+            builder.Services.AddSingleton<IApiKeyProvider, ApiKeyProvider>();
+            builder.Services.AddSingleton<ILocationProvider, LocationProvider>();
+            builder.Services.AddSingleton<IWeatherClientCollection>(sp =>
+            {
+                var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+                var apiKeyProvider = sp.GetRequiredService<IApiKeyProvider>();
+                var clientCache = new WeatherClientCollection(httpClientFactory, apiKeyProvider.GetApiKey());
+
+                var locProvider = sp.GetRequiredService<ILocationProvider>();
+                var locations = locProvider.GetAll();
+
+                foreach (var loc in locations)
+                    clientCache.AddClient(loc, TimeSpan.FromMinutes(5));
+
+                return clientCache;
+            });
 
             var app = builder.Build();
 
@@ -23,20 +45,21 @@ namespace TimeAndTemperature
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("/Error", createScopeForErrors: true);
+                // The default HSTS value is 30 days. You may want to change this for production scenarios,
+                // see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
 
+            app.UseStaticFiles();
             app.UseAntiforgery();
 
-            app.MapStaticAssets();
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode()
                 .AddInteractiveWebAssemblyRenderMode()
-                .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+                .AddAdditionalAssemblies(typeof(TimeAndTemperature.Client._Imports).Assembly);
 
             app.Run();
         }
